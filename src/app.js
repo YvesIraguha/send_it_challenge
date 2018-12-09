8bda21664baa999417e1d8d475cf55ae3c7cb8e9
YvesIraguha <iraguhaivos@gmail.com>
2018-12-09 10:10:17 +0200

    [chore #162527184] Fetch data from API

--- src/app.js
+++ src/app.js
@@ -2,18 +2,26 @@ import express from 'express';
 import dotenv from 'dotenv';
 import parcelsRouter from './routes/parcels';
 import usersRouter from './routes/users';
+import pages from './routes/staticFilesRoutes';
+import path from 'path';
 
 dotenv.config();
 // instantiate the app;
 const app = express();
 // set the middle ware to use for body parsing
+
+//Set the static directory to use 
+app.use(express.static(path.join(__dirname, "../UI")));
+
+//set the middlewares 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // set the router to use.
 app.use('/api/v1', parcelsRouter);
 app.use('/api/v1/users', usersRouter);
+app.use('/pages',pages);
 app.get('/', (req, res) => {
-  res.send({message:"Welcome to yvessendit api",structure:"HTTP method: /api/v1/"});
+  res.redirect('https://documenter.getpostman.com/view/6026755/RzfdpVau');
 });
  app.get('*',(req,res) => {
    res.status(404).send({message:"Page requested not found"});
