a3c25e80a309877d771c1c9c42e304fa02a328e6
YvesIraguha <iraguhaivos@gmail.com>
2019-03-09 16:26:23 +0200

    [gh-pages remove comments]

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -51,50 +51,6 @@ form.addEventListener('submit', function(event) {
   return false;
 });
 
-// const button = document.querySelector('.signup');
-// button.onclick = function() {
-//   const firstname = document.querySelector('.input-firstname').value;
-//   const lastname = document.querySelector('.input-lastname').value;
-//   const email = document.querySelector('.input-email').value;
-//   const phone = document.querySelector('.input-phone').value;
-//   const password = document.querySelector('.input-password').value;
-//   const error = document.querySelector('.error');
-//   if (verify(firstname, lastname, phone, email, password)) {
-//     return;
-//   } else {
-//     const data = {
-//       firstname,
-//       lastname,
-//       phone,
-//       email,
-//       password
-//     };
-
-//     fetch('/api/v1/users/signup', {
-//       method: 'POST',
-//       headers: new Headers({
-//         'Content-Type': 'application/json'
-//       }),
-//       body: JSON.stringify(data)
-//     })
-//       .then(response => response.json())
-//       .then(myJson => {
-//         if (myJson.error) {
-//           error.innerHTML = myJson.error;
-//         } else {
-//           error.innerHTML = '';
-//           localStorage.setItem('token', myJson.token);
-//           localStorage.setItem('userid', myJson.response.id);
-//           localStorage.setItem('username', myJson.response.firstname);
-//           window.location = '/pages/user';
-//         }
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   }
-// };
-
 function verify(firstname, lastname, phone, email, password, cfpassword) {
   const error = document.querySelector('.error');
   const firstnameError = document.querySelector('#firstname_error');
